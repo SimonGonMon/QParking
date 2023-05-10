a990326b36b1a5fe560d5e7d93b5cd5a33175d77
simon.gon.mon <simon.gon.mon@gmail.com>
2023-05-09 23:20:37 -0500

    Rework Landing Page

--- resources/views/layouts/app-master.blade.php
+++ resources/views/layouts/app-master.blade.php
@@ -1,11 +1,14 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Hugo 0.87.0">
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
+    <!-- Favicon-->
+    <!-- Bootstrap icons-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet"/>
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="css/styles.css" rel="stylesheet"/>
+
     <title>QParking</title>
 
     <!-- Bootstrap core CSS -->
@@ -35,7 +38,7 @@
 
 @include('layouts.partials.navbar')
 
-<main class="container">
+<main class="container-fluid p-0">
     @yield('content')
 </main>
 
