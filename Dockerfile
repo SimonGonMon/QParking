7e426f5675910718b37d431739313526f7a340d5
simon.gon.mon <simon.gon.mon@gmail.com>
2023-05-16 10:31:57 -0500

    Refactor `Dockerfile` & `start.sh`

--- Dockerfile
+++ Dockerfile
@@ -23,6 +23,6 @@ WORKDIR /app
 
 RUN composer install
 
-CMD php artisan serve --host=0.0.0.0 --port=80
+CMD sh start.sh
 
 EXPOSE 80
---
+++ start.sh
@@ -0,0 +1,3 @@
+eval $(printenv | sed -n "s/^\([^=]\+\)=\(.*\)$/export \1=\2/p" | sed 's/"/\\\"/g' | sed '/=/s//="/' | sed 's/$/"/' >> /etc/profile)
+
+php artisan serve --host=0.0.0.0 --port=80
