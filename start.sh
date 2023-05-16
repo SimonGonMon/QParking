c2305e256cc43d35ebec97197fa03a78b1760c83
simon.gon.mon <simon.gon.mon@gmail.com>
2023-05-16 10:50:35 -0500

    Fix .env try

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
+# Esta es la mayor payasada vista nunca. Carga las variables de entorno del contenedor externo de Azure en el contenedor interno de Docker
 eval $(printenv | sed -n "s/^\([^=]\+\)=\(.*\)$/export \1=\2/p" | sed 's/"/\\\"/g' | sed '/=/s//="/' | sed 's/$/"/' >> /etc/profile)
 
+# Optimizamos el framework
+php artisan optimize
+
+# Corremos el servidor de Laravel
 php artisan serve --host=0.0.0.0 --port=80
